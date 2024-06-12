1cd1a71098b7e28b12f40cf9f4c9244797bea522
zing <v_zxxzyao@tencent.com>
2024-06-12 20:33:57 +0800

    base code

--- content.js
+++ content.js
@@ -6,16 +6,23 @@ window.addEventListener('load', () => {
     const $ = layui.$;
     const copyBtn = $('<button class="layui-btn plugin-copy-btn">插件复制</button>')
 
-    // 为复制按钮添加点击事件
-    $('.plugin-copy-btn').click(() => {
-      layer.msg('tap')
-    })
     const codeTags = $('code')
 
     // 遍历code标签
     codeTags.each((_, dom) => {
       $(dom).hover(() => {
         $(dom).append(copyBtn);
+        // 为复制按钮添加点击事件
+        $('.plugin-copy-btn').click((dom) => {
+          console.log('dom',$(dom))
+          const target = $(dom).parent()
+          target.focus();
+          const range = document.createRange();
+          const selection = window.getSelection();
+          range.selectNodeContents(target[0]);
+          selection.removeAllRanges();
+          selection.addRange(range);
+        })
       }, () => {
         $('.plugin-copy-btn').remove();
       })
